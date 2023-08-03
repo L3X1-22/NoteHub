2002cba089a231d9ecda41aebd50033d184ee6ce
L3X1-22 <alexia300622@gmail.com>
2023-08-02 20:53:26 -0500

    funtion to read notes created

--- Main.py
+++ Main.py
@@ -1,8 +1,12 @@
 import DB.prep
 import DB.cnx as database
 import Users.manager
+import Notes.manager
 
 #DB.prep.prep()
 
 newUser = Users.manager.user('davis', 'travis', 'travis@travis.com', '1234')
 newUser.register()
+
+note = Notes.manager.notes(1)
+note.readNotes()
--- Notes/manager.py
+++ Notes/manager.py
@@ -1,10 +1,19 @@
 import DB.cnx as cnx
+import DB.prep as prep
 
 class notes():
     def __init__(self, userID):
         self.userID = userID
 
     def readNotes(self):
+
+        #data and sql code for query
+        sql = "SELECT * FROM notes WHERE userID = %s"
+        data = (self.userID)
+
+        prep.use_DB()
+        cnx.cursor.execute(sql, data)
+        print (cnx.cursor.fetchall())
         return None
     
     def addNotes(self):
