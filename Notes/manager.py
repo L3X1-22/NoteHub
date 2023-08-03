80a3ed7eeac1f93ad3b641408a3147ef6272b83d
L3X1-22 <alexia300622@gmail.com>
2023-08-03 00:31:05 -0500

    created delNote method

--- Notes/manager.py
+++ Notes/manager.py
@@ -10,15 +10,35 @@ class notes():
         #sql code for query
         sql = f"SELECT * FROM notes WHERE userID = {self.userID}"
 
+        #execute sql query
         prep.use_DB()
         cnx.cursor.execute(sql)
         print (cnx.cursor.fetchall())
+
         return None
     
-    def addNotes(self):
+    def addNotes(self, title, body):
+
+        #sql code and data for query
+        sql = "INSERT INTO notes VALUES(null, %s, %s, %s)"
+        data = (title, body, self.userID)
+
+        #execute sql query
+        prep.use_DB()
+        cnx.cursor.execute(sql, data)
+        cnx.cnx.commit()
+
         return None
 
-    def delNotes(self):
+    def delNotes(self, note):
+
+        #sql code and data for query
+        sql =   f"DELETE FROM notes WHERE ID = {note} AND userID = {self.userID};"
+
+        #execute sql query
+        prep.use_DB()
+        cnx.cursor.execute(sql)
+        cnx.cnx.commit()
         return None
     
     def modNotes(self):
