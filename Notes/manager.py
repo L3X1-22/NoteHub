027662a46526956cd482a00a80b80713851db18b
L3X1-22 <alexia300622@gmail.com>
2023-08-02 20:12:44 -0500

    created methods to manage notes

---
+++ Notes/manager.py
@@ -0,0 +1,17 @@
+import DB.cnx as cnx
+
+class notes():
+    def __init__(self, userID):
+        self.userID = userID
+
+    def readNotes(self):
+        return None
+    
+    def addNotes(self):
+        return None
+
+    def delNotes(self):
+        return None
+    
+    def modNotes(self):
+        return None
